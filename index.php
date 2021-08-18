9a73704c5c16db3673dbfc898ad6655a407f6718
Nathanael Zuchuon <nzuchuon@gmail.com>
2021-08-18 12:16:29 +0100

    Add helpers

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '\core\Model.php';
 require_once __DIR__ . '\core\Router.php';
 require_once __DIR__ . '\core\Controller.php';
 require_once __DIR__ . '\core\errorHandler.php';
+require_once __DIR__ . '\helpers\passwordEncryption.php';
 
 require './controllers/indexController.php';
 
